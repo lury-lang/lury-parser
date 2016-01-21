b3fdd64d4b6be26c0652be6091ed77e9302c3bf1
Tomona Nanase <nanase@users.noreply.github.com>
2016-01-21 17:02:59 +0900

    add: ParsingSuccess.csを追加

---
+++ UnitTest/ParsingSuccess.cs
@@ -0,0 +1,38 @@
+﻿using Lury.Compiling.Lexer;
+using Lury.Compiling.Parser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ParsingSuccess
+    {
+        [TestMethod]
+        public void Success()
+        {
+            var testcases = SuccessResource.ResourceManager.GetResourceSet(CultureInfo.InvariantCulture, true, true)
+                .OfType<DictionaryEntry>()
+                .Where(e => ((string)e.Key).StartsWith("Success"));
+
+            foreach (var testcase in testcases)
+            {
+                var lexer = new Lexer("", (string)testcase.Value);
+                lexer.Tokenize();
+
+                var parser = new Parser(lexer.TokenOutput);
+
+                parser.Parse();
+
+                Assert.IsTrue(parser.IsFinished);
+                Assert.IsNotNull(parser.TreeOutput);
+                Assert.AreEqual(0, parser.Logger.Outputs.Count());
+                
+                Debug.WriteLine("Passed: {0} (in: {1})", (string)testcase.Key);
+            }
+        }
+    }
+}
